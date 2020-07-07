8a182779709d3b9a187ad38f985aa6026df6583e
AaronPoon <aironpoon@gmail.com>
2020-07-06 22:10:28 -0700

    Enhance UI with @ConfigSection Annotations

--- src/test/java/com/essencerunning/EssenceRunningPluginTest.java
+++ src/test/java/com/essencerunning/EssenceRunningPluginTest.java
@@ -3,11 +3,9 @@ package com.essencerunning;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class EssenceRunningPluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(EssenceRunningPlugin.class);
-		RuneLite.main(args);
-	}
+public class EssenceRunningPluginTest {
+    public static void main(String[] args) throws Exception {
+        ExternalPluginManager.loadBuiltin(EssenceRunningPlugin.class);
+        RuneLite.main(args);
+    }
 }
